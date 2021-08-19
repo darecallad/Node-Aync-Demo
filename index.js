33a24bf6b45bdfd6e6a5901e751a2958dcb2c5bd
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-19 16:09:19 -0700

    Callback function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 console.log("Before");
-getUser(1);
+getUser(1, (user) => {
+  console.log("User", user);
+
+  // Get the repositories
+  getRepo(user.gitHubUsername, (repo) => {
+    console.log("Getting Repo");
+    console.log(repo);
+  });
+});
 // cannot do const user = getUser(1);
 console.log("After");
 
@@ -7,9 +15,17 @@ console.log("After");
 //Promises
 //Async/await
 
-function getUser(id) {
+//Dealing with Callback function
+function getUser(id, callback) {
   setTimeout(() => {
     console.log("Reading a user from a database.");
-    return { id: id, gitHubUsername: "John" };
+    callback({ id: id, gitHubUsername: "John" });
+  }, 2000);
+}
+
+function getRepo(username, callback) {
+  setTimeout(() => {
+    console.log("Loading Repo for :" + username);
+    callback(["repo1", "repo2", "repo3"]);
   }, 2000);
 }
