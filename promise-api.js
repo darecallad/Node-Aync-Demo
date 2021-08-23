515eec784ce636e006b6cdb506eacc40e2daed49
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-23 15:57:46 -0700

    Promise-api

--- promise-api.js
+++ promise-api.js
@@ -3,3 +3,20 @@ p.then((result) => console.log(result));
 
 // catch to get the error
 // then to get the resolve
+
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async operation 1");
+    resolve(1);
+  }, 2000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async operation 2");
+    resolve(2);
+  }, 2000);
+});
+// Promise.race  will return the first value to fulfilled
+Promise.all([p1, p2]).then((result) => console.log(result));
+// if any promise rejected the result will showing reject
